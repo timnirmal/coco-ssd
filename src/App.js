5e3343c75a707ef8195bdcb150dc1e57c4a99501
thimira nirmal <timnirmal@gmail.com>
2021-10-18 20:51:13 +0530

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //Dependencies
 import React, {useEffect, useRef} from "react";
-import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import './App.css';
